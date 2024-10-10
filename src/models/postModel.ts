733788e80b92a48a1703b055aaa7108e12238f45
Gad <you@example.com>
2024-10-10 12:09:50 +0300

    finished the models

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -1,20 +1,61 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
+import { IUser } from "./userModel";
 
 export interface IComment {
   content: string;
   author: Types.ObjectId;
   createdAt: Date;
-}
+  post: IPost['_id']
+};
 
 export interface IPost extends Document {
-  _id: Types.ObjectId;
   title: string;
   content: string;
   author: Types.ObjectId;
   comments: IComment[];
-}
+  user: IUser['_id']
+};
 
-const CommentSchema = new Schema<IComment>({});
 
+const CommentSchema = new Schema<IComment>({
+  content: {
+    type: String,
+    require: true,
+  },
+
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'post',
+    require: true,
+  },
+
+  createdAt: {
+    type: Date,
+    require: true
+},
+});
+
+const PostSchema: Schema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+
+  content: {
+    type: String,
+    require: true,
+  },
+
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    require: true,
+  },
+
+  comments: {
+    type: [CommentSchema],
+    require: false,
+  }
+});
 
 export default mongoose.model<IPost>("Post", PostSchema);
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 import validator from "validator";
+import { IPost } from './postModel';
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
   username: string;
@@ -10,7 +12,8 @@ export interface IUser extends Document {
     socialLinks?: string[];
   };
   posts: Types.ObjectId[];
-}
+  comparePassword(userPassword: string): Promise<Boolean>
+};
 
 const UserSchema: Schema = new Schema({
   username: {
@@ -30,11 +33,40 @@ const UserSchema: Schema = new Schema({
     require: true,
     unique: true,
     lowercase: true,
+    validate: [validator.isEmail, 'Email is not valid'],
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
 },
 
+profile: {
+  bio:{
+    type: String,
+    default: '',
+  },
+  socialLinks:{
+    type: [String],
+    required: false,
+    validate: [validator.isURL, 'Invalid URL in social links'],
+  }
+},
+
+posts: {
+  type: [Schema.Types.ObjectId],
+  ref: 'posts'
+}
 }, { timestamps: true });
 
 
+//פונקצייה שמצפינה את הסיסמא
+UserSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+//מתודה להשוואת סיסמאות
+UserSchema.methods.comparePassword = async function (userPassword: string): Promise<boolean> {
+  return bcrypt.compare(userPassword, this.password)
+}
 
 export default mongoose.model<IUser>("User", UserSchema);
